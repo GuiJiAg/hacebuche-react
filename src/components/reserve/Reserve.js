7f5626fffb07de9347a1b58a3247b679a4b38bf7
GuiJiAg <guillermojjagudo@gmail.com>
2019-06-11 18:28:33 +0200

    Commit

--- src/components/reserve/Reserve.js
+++ src/components/reserve/Reserve.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import reservationImg from '../../images/reservation.jpg';
+import axios from 'axios';
 import './Reserve.css';
 
 function Header() {
@@ -25,50 +26,99 @@ class CardForm extends React.Component {
         <p>
           <label>Nombre</label>
         </p>
-        <input className="w3-input border w3-round-large w3-animate-input" type="text" />   
+        <input 
+          className="w3-input border w3-round-large w3-animate-input" 
+          name="name"
+          type="text" 
+          value={this.props.inputsValue.name}
+          onChange={this.props.onChange}
+        />   
         <br />
         <p>  
           <label>Correo electrónico</label>
         </p>
-        <input className="w3-input border w3-round-large w3-animate-input" type="text" />
+        <input 
+          className="w3-input border w3-round-large w3-animate-input" 
+          name="email"
+          type="text" 
+          value={this.props.inputsValue.email}
+          onChange={this.props.onChange}
+        />
         <br />
         <p>  
           <label>Teléfono</label>
         </p>
-        <input className="w3-input border w3-round-large w3-animate-input" type="text" />
+        <input 
+          className="w3-input border w3-round-large w3-animate-input" 
+          name="phone"
+          type="text" 
+          value={this.props.inputsValue.phone}
+          onChange={this.props.onChange}
+        />
         <br />
         <p>  
           <label>¿Para qué día?</label>
         </p>
-        <input className="w3-input border w3-round-large" type="date" />
+        <input 
+          className="w3-input border w3-round-large" 
+          name="reserveDate"
+          type="date" 
+          value={this.props.inputsValue.reserveDate}
+          onChange={this.props.onChange}
+        />
         <br />
         <p>  
           <label>¿A qué hora?</label>
         </p>
-        <select className="border">
-          <option>13:30</option>
-          <option>14:00</option>
-          <option>14:30</option>
-          <option>15:00</option>
-          <option>21:30</option>
-          <option>22:00</option>
-          <option>22:30</option>
-          <option>23:00</option>
+        <select 
+          className="border"
+          name="reserveHour"
+          value={this.props.inputsValue.reserveHour}
+          onChange={this.props.onChange}
+          >
+          <option value="13:30">13:30</option>
+          <option value="14:00">14:00</option>
+          <option value="14:30">14:30</option>
+          <option value="15:00">15:00</option>
+          <option value="21:30">21:30</option>
+          <option value="22:00">22:00</option>
+          <option value="22:30">22:30</option>
+          <option value="23:00">23:00</option>
         </select>
         <br />
         <p>  
           <label>¿Cuántas personas?</label>
         </p>
-        <input className="w3-input border w3-round-large w3-animate-input" type="text" />
+        <input 
+          className="w3-input border w3-round-large w3-animate-input" 
+          name="numberDiners"
+          type="text" 
+          value={this.props.inputsValue.numberDiners}
+          onChange={this.props.onChange}
+        />
         <br />
         <p>  
           <label>¿Dónde prefieres?</label>
         </p>
         <div>
-          <input className="w3-radio" type="radio" name="preferedPlace" value="Terraza" checked="checked" />
+          <input 
+            className="w3-radio"
+            type="radio" 
+            name="preferedPlace" 
+            value="Terraza" 
+            checked={this.props.inputsValue.preferedPlace === 'Terraza'}
+            onChange={this.props.onChange}
+          />
           <label className="radioLabel">Terraza</label>
           <br />
-          <input className="w3-radio" type="radio" name="preferedPlace" value="Local" />
+          <input 
+            className="w3-radio" 
+            type="radio" 
+            name="preferedPlace" 
+            value="Local" 
+            checked={this.props.inputsValue.preferedPlace === 'Local'}
+            onChange={this.props.onChange}
+          />
           <label className="radioLabel">Local</label>
         </div>
         <br />
@@ -77,7 +127,12 @@ class CardForm extends React.Component {
           <br />
           <label>(Una hora más específica, alergias, productos para celíacos,...)</label>
         </p>
-        <textarea className="border" name="observations"></textarea> 
+        <textarea 
+          className="border" 
+          name="observations"
+          value={this.props.inputsValue.observations}
+          onChange={this.props.onChange}>
+        </textarea> 
         <hr />
       </form>
     );
@@ -88,7 +143,10 @@ class CardFormButton extends React.Component {
   render() {
     return (
       <div className="button-container w3-container">
-        <button className="w3-xlarge w3-round-large">
+        <button 
+          className="w3-xlarge w3-round-large"
+          onClick={this.props.onClick}
+        >
           Enviar
         </button>
       </div>
@@ -101,8 +159,13 @@ class Card extends React.Component {
     return (
       <div className="w3-card">
         <CardHeader />
-        <CardForm />
-        <CardFormButton />
+        <CardForm
+          onChange={this.props.onChange}
+          inputsValue={this.props.inputsValue}
+        />
+        <CardFormButton 
+          onClick={this.props.onClick}
+        />
       </div>
     );
   }
@@ -141,7 +204,7 @@ function ModalErrorHeader() {
 class ModalErrorContent extends React.Component {
   render() {
     return (
-      <div class="w3-container">
+      <div className="w3-container">
         <p>Uno o más datos introducidos no son correctos:</p>
         <br />
         <hr />
@@ -198,12 +261,63 @@ class ModalCorrect extends React.Component {
 }
 
 class Reserve extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'',
+      email:'',
+      phone:'',
+      reserveDate:'',
+      reserveHour:'13:30',
+      numberDiners:'',
+      preferedPlace:'',
+      observations:''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    const input = event.target.name;
+    let value = '';
+    
+    if (input === 'phone' || input === 'numberDiners') {
+      value = parseInt(event.target.value);
+    }
+    else {
+      value = event.target.value;
+    }
+
+    this.setState({
+      [input]: value
+    });
+  }
+
+  handleClick() {
+    const url = 'https://hacebuche-api.herokuapp.com/api/mail'
+    let requestHeaders = {
+      'Content-Type':'application/json '
+    };
+
+    axios.post(url, this.state, { headers: requestHeaders })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div>
         <Header />
         <div className="w3-content w3-padding-64">
-          <Card />
+          <Card
+            onChange={this.handleChange}
+            onClick={this.handleClick}
+            inputsValue={this.state}
+          />
           <IframeHaceBOT />
           <ModalError />
           <ModalCorrect />
